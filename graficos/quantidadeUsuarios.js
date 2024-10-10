17f5fab1f9578352b7be5f8de70a1e641248b6bf
AndreyTM2 <106534302+AndreyTM2@users.noreply.github.com>
2024-10-10 09:08:53 -0300

    minecraft

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,64 @@
+import { getCSS, tickConfig, criarGrafico } from "./common.js";
+
+async function quantidadeUsuariosPorRede() {
+  //const url = ''
+  //const res = await fetch(url)
+  //const dados = await res.json()
+  const dados = JSON.stringify({
+    Facebook: 78,
+    Youtube: 4124,
+    WhatsApp: 67,
+    Instagram: 234,
+    Tiktok: 12,
+    WeChat: 235,
+  });
+  const nomeDasRedes = Object.keys(dados);
+  const quantidadeDeUsuarios = Object.values(dados);
+
+  const data = [
+    {
+      x: nomeDasRedes,
+      y: quantidadeDeUsuarios,
+      type: "bar",
+      marker: {
+        color: getCSS("--primary-color"),
+      },
+    },
+  ];
+
+  const layout = {
+    plot_bgcolor: getCSS("--bg-color"),
+    paper_bgcolor: getCSS("--bg-color"),
+    title: {
+      text: "Redes sociais com mais usuários no mundo",
+      x: 0,
+      font: {
+        color: getCSS("--primary-color"),
+        family: getCSS("--font"),
+        size: 30,
+      },
+    },
+    xaxis: {
+      tickfont: tickConfig,
+      title: {
+        text: "nome das redes sociais",
+        font: {
+          color: getCSS("--secondary-color"),
+        },
+      },
+    },
+    yaxis: {
+      tickfont: tickConfig,
+      title: {
+        text: "bilhões de usuários ativos",
+        font: {
+          color: getCSS("--secondary-color"),
+        },
+      },
+    },
+  };
+
+  criarGrafico(data, layout);
+}
+
+quantidadeUsuariosPorRede();
